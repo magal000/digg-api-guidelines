0b00df680b1c9361353456374b3e658b6af6f24b
Fredrik Nordlander <fredrik.nordlander@digg.se>
2023-11-29 21:36:14 +0100

    Uppdaterat med modifierade regler samt modifierade testfall

--- rulesets/FnsRules.ts
+++ rulesets/FnsRules.ts
@@ -1,16 +1,16 @@
 import { RulesetInterface } from "../ruleinterface/RuleInterface.ts"
-import { enumeration, truthy, falsy, undefined as undefinedFunc, pattern, schema } from "@stoplight/spectral-functions";
+import { enumeration, truthy, falsy, undefined as undefinedFunc, pattern, schema, casing } from "@stoplight/spectral-functions";
 import { DiagnosticSeverity } from "@stoplight/types";
 
 
 export class Fns01 implements RulesetInterface {
     description = "Parameternamn SKALL anges med en konsekvent namnkonvention inom ett API, exempelvis antingen snake_case eller camelCase";
     message = "{{property}}--> Parameternamn SKALL anges med en konsekvent namnkonvention exempelvis antingen snake_case eller camelCase";
-    given = "$.paths...parameters[?(@.in=='query')].name";
+    given = "$.paths.*.*.parameters[?(@.in=='query')].name";
     then = {
       function: pattern,
       functionOptions: {
-      match:  "^([a-z]+[A-Z]+\\w+)+|^[a-z]+(?:_[a-z]+)*$"
+        match: "^(?:[a-z0-9]+(?:_[a-z0-9]+)*|[a-z]+(?:[A-Z][a-z]*)*)$"
       }
     }
   severity = DiagnosticSeverity.Error;
--- tests/fns.test.ts
+++ tests/fns.test.ts
@@ -3,30 +3,108 @@ import testRule from "./util/helperTest.ts";
 
 testRule("Fns01", [
   {
-      name: "giltigt testfall",
-      document: {
-        openapi: "3.1.0",
-        info: { version: "1.0" },
-        paths: { "/camelCase": {} },
+    name: "giltigt testfall",
+    document: {
+      openapi: "3.1.0",
+      info: { version: "1.0" },
+      paths: {
+        "/foo": {
+          get: {
+            description: "Gilitigt testfall av camelCase",
+            parameters: [
+              {
+                name: "veryLongName",
+                in: "query",
+                required: false,
+              },
+            ],
+          },
+        },
       },
-      errors: [],
     },
-      {
-      name: "ogiltigt testfall",
-      document: {
-        openapi: "3.1.0",
-        info: { version: "1.0" },
-        paths: { "/pets": {} },
-        parameters: [{ name: "Thisisnotcamelcase", in: "query"}]
-     },
-      errors: [
-        {
-          message: "Thisisnotcamelcase Parameternamn SKALL anges med en konsekvent namnkonvention exempelvis antingen snake_case eller camelCase",
-          path: ["parameters", "0", "name"],
-          severity: DiagnosticSeverity.Error,
-        }
-      ],
-    },  
-  ]);
+    errors: [],
+  },
 
+  {
+    name: "ogiltigt testfall",
+    document: {
+      openapi: "3.1.0",
+      info: { version: "1.0" },
+      paths: {
+        "/foo": {
+          get: {
+            description: "Ogiltigt testfall av CamelCase",
+            parameters: [
+              {
+                name: "VeryLongName",
+                in: "query",
+                required: false,
+              },
+            ],
+          },
+        },
+      },
+    },
+    errors: [
+      {
+        message:
+          "name--> Parameternamn SKALL anges med en konsekvent namnkonvention exempelvis antingen snake_case eller camelCase",
+        path: ["paths", "/foo", "get", "parameters", "0","name"],
+        severity: DiagnosticSeverity.Error,
+      },
+    ],
+  },
+  {
+    name: "giltigt testfall",
+    document: {
+      openapi: "3.1.0",
+      info: { version: "1.0" },
+      paths: {
+        "/foo": {
+          get: {
+            description: "Gilitigt testfall av snake case",
+            parameters: [
+              {
+                name: "very_long_name",
+                in: "query",
+                required: false,
+              },
+            ],
+          },
+        },
+      },
+    },
+    errors: [],
+  },
+
+  {
+    name: "ogiltigt testfall",
+    document: {
+      openapi: "3.1.0",
+      info: { version: "1.0" },
+      paths: {
+        "/foo": {
+          get: {
+            description: "Ogiltigt testfall av snake case",
+            parameters: [
+              {
+                name: "very_longName",
+                in: "query",
+                required: false,
+              },
+            ],
+          },
+        },
+      },
+    },
+    errors: [
+      {
+        message:
+          "name--> Parameternamn SKALL anges med en konsekvent namnkonvention exempelvis antingen snake_case eller camelCase",
+        path: ["paths", "/foo", "get", "parameters", "0","name"],
+        severity: DiagnosticSeverity.Error,
+      },
+    ],
+  },  
+]);
   
