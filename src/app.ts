5d78a4314ad1ec657e6d1221d611d5d8c213f72f
Fredrik Nordlander <fredrik.nordlander@digg.se>
2023-12-08 09:40:54 +0100

    Lagt till stöd för RAP-LP att skriva output till logfiler

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,10 @@ import { join } from "path";
 import Parsers from "@stoplight/spectral-parsers";
 import spectralCore from "@stoplight/spectral-core";
 import { importAndCreateRuleInstances, getRuleModules } from "./util/ruleUtil.ts"; // Import the helper function
+import util from 'util';
 const { Spectral, Document } = spectralCore;
+const writeFileAsync = util.promisify(fs.writeFile);
+const appendFileAsync = util.promisify(fs.appendFile);
 
 try {
   // Parse command-line arguments using yargs
@@ -31,12 +34,23 @@ try {
       alias: "c",
       describe: `Rule categories separated by commas.\nAvailable categories:\r ${getRuleModules().join(",")}`,
       type: "string",
+    })
+    .option('log', {
+      alias: 'l',
+      describe: 'Path to the log file. If not provided, logs will be printed to stdout.',
+      type: 'string',
+    })
+    .option("append", {
+      alias: "a",
+      describe: "Append log information to the log file (if specified).",
+      type: "boolean",
+      default: false,
     }).argv;
 
   // Extract arguments from yargs
   const apiSpecFileName = (argv.file as string) || "";
   const ruleCategories = argv.categories ? (argv.categories as string).split(",") : undefined;
-
+  const logFilePath = argv.log as string | undefined;
   try {
     // Import and create rule instances in RAP-LP
     const enabledRules = await importAndCreateRuleInstances(ruleCategories);
@@ -56,7 +70,22 @@ try {
     try {
       // Run Spectral on the API specification and log the result
       const result = await spectral.run(apiSpecDocument);
-      console.log(result);
+      //Check specified option from yargs input
+      if (logFilePath) {
+
+        if (argv.append) {
+          await appendFileAsync(logFilePath,JSON.stringify(result,null,2));
+          console.log(`Appending linting results from RAP-LP to ${logFilePath}`);
+        }else {
+        //Log to disc
+        await writeFileAsync(logFilePath,JSON.stringify(result,null,2));
+        console.log(`Writing linting results from RAP-LP to ${logFilePath}`);
+        }
+      }else {
+        //Log to stdout
+        console.log(JSON.stringify(result,null,2));
+      }
+      //console.log(result);
     } catch (spectralError: any) {
       console.error("Error running Spectral:", spectralError.message);
     }
