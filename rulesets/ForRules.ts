19182ef7498b935f24575696b53b13eb8ae05b29
Fredrik Nordlander <fredrik.nordlander@digg.se>
2024-02-23 07:57:05 +0100

    Lagt till regel samt testfall för FOR.02

--- rulesets/ForRules.ts
+++ rulesets/ForRules.ts
@@ -1,4 +1,4 @@
-import { enumeration, falsy, undefined as undefinedFunc, pattern } from "@stoplight/spectral-functions";
+import {undefined as undefinedFunc } from "@stoplight/spectral-functions";
 import { DiagnosticSeverity } from "@stoplight/types";
 import { BaseRuleset, CustomProperties } from "./BaseRuleset.ts"
 import { oas3 } from "@stoplight/spectral-formats";
@@ -7,6 +7,7 @@ import { oas2 } from "@stoplight/spectral-formats";
 /**
  * Module contains classes with functions that are need
  */
+/*
 export class For02 extends BaseRuleset {
     static customProperties: CustomProperties = {
       område: "Förutsättningar",
@@ -24,4 +25,20 @@ export class For02 extends BaseRuleset {
       severity = DiagnosticSeverity.Error
       
   }
-  export default { For02 };
+*/
+export class For02 extends BaseRuleset {
+  static customProperties: CustomProperties = {
+    område: "Förutsättningar",
+    id: "FOR.02",
+  };
+  description = "EN GET -förfrågan SKALL INTE acceptera en body";
+  message = "EN GET -förfrågan SKALL INTE acceptera en body";
+  given = "$.paths..get.requestBody";
+  then =  {
+    function: undefinedFunc,
+  }
+  //formats = [oas3]
+    severity = DiagnosticSeverity.Error
+    
+}
+export default { For02 };
--- tests/for.test.ts
+++ tests/for.test.ts
@@ -2,56 +2,47 @@ import { DiagnosticSeverity } from "@stoplight/types";
 import testRule from "./util/helperTest.ts";
 
 testRule("For02", [
-    {
-      name: "giltigt testfall",
-      document: {
-        openapi: "3.1.0",
-        info: { version: "1.0" },
-        paths: {
-          "/bodycheck": {
-            get: {
-              description: "EN GET -förfrågan SKALL INTE acceptera ett body",
-              parameters: [
-                {
-                  name: "veryLongName",
-                  in: "query",
-                  required: false,
-                },
-              ],
-            },
-          },
+  {
+    name: "giltigt testfall - EN GET -förfrågan SKALL INTE acceptera en  body",
+    document: {
+      openapi: "3.1.0",
+      info: { version: "1.0" },
+      paths: {
+        "/": {
+          get: {},
         },
       },
-      errors: [],
     },
-  
-    {
-      name: "ogiltigt testfall",
-      document: {
-        openapi: "3.1.0",
-        info: { version: "1.0" },
-        paths: {
-          "/bodycheck": {
-            get: {
-              description: "EN GET -förfrågan SKALL INTE acceptera ett body",
-              parameters: [
-                {
-                  name: "body",
-                  in: "query",
-                  required: false,
+    errors: [],
+  },
+  {
+    name: "ogiltigt testfall - EN GET -förfrågan SKALL INTE acceptera en body",
+    document: {
+      openapi: "3.1.0",
+      info: { version: "1.0" },
+      paths: {
+        "/": {
+          get: {
+            requestBody: {
+              description: "En GET operation är en fråga och skall inte innehålla något svar",
+              content: {
+                "application/json": {
+                  schema: {
+                    type: "object",
+                  },
                 },
-              ],
+              },
             },
           },
         },
       },
-      errors: [
-        {
-          message:
-            "EN GET -förfrågan SKALL INTE acceptera ett body",
-          path: ["paths", "/bodycheck", "get", "parameters", "0","name"],
-          severity: DiagnosticSeverity.Error,
-        },
-      ],
-    }
+    },
+    errors: [
+      {
+        message: "EN GET -förfrågan SKALL INTE acceptera en body",
+        path: ["paths", "/", "get", "requestBody"],
+        severity: DiagnosticSeverity.Error,
+      },
+    ],
+  }  
   ]);
