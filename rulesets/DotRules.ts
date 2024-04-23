25301bd5836e2ad9240518e314c56f5f5762de45
Fredrik Nordlander <fredrik.nordlander@digg.se>
2024-04-23 13:58:17 +0200

    Lagt till regler och villkor för DOT.03 samt ändrat och utökat implementation av DOT.02

--- rulesets/DotRules.ts
+++ rulesets/DotRules.ts
@@ -1,92 +1,95 @@
 import { enumeration, truthy, falsy, undefined as undefinedFunc, pattern, schema } from "@stoplight/spectral-functions";
 import { DiagnosticSeverity } from "@stoplight/types";
 import { CustomProperties } from '../ruleinterface/CustomProperties.ts';
-import { BaseRuleset} from "./BaseRuleset.ts"
-import { RulesetFunctionContext,IFunctionResult } from "@stoplight/spectral-core";
-import { parseProperties,Property,isValidRFC3339DateTime } from "./rulesetUtil.ts";
+import { BaseRuleset} from "./BaseRuleset.ts";
+import { parseProperties,Property} from "./rulesetUtil.ts";
+import { DotRuleBase,DotStateExecutionLog} from "./util/DotRulesUtil.ts";
 
 const moduleName: string = "DotRules.ts";
 
-interface DotState {
-    schemaKey: string,
-    propertyName: string,
-    propertyType:string,
-    propertyFormat:string,
-    propertyExample: string,
-    propertyValidFormatDate: boolean,
-    propertyValidFormatDateTime: boolean
-}   
-interface DotStateExecutionLog {
-    [key: string]: DotState[];
-}
-export class Dot02 extends BaseRuleset {
-    static customProperties: CustomProperties = {
-        område: "Datum- och tidsformat",
-        id: "DOT.02",
-      };
-      description = "Ett giltigt exempel enligt DOT.02 behöver anges som ett exempel";
-      message = "Datum och tid SKALL anges enligt RFC 3339 som bygger på ISO-8601.";
-      given = "$.components.schemas";
-      then = {
-          function: (targetVal: any, _opts: string, paths) => {
-    
-              const result:any = [];
-              let isTrackRuleExecution = false;
-              const dotStateExecutionLogDictionary: DotStateExecutionLog = {};
-              //const data = JSON.parse(JSON.stringify(targetVal,null,2));  // Need to use both stringfy and parse in order to parse to JSON correctly here...hm ?
-              const data = targetVal;
-              for (const key in data ) { // Loop throough each Schema object found in given expression
-                const propertys: Property[] = parseProperties(key,data); // Pick out properties of object
-                propertys.forEach(prop => {
-                  const stateKey = `${key}:${prop.name}:${Math.floor(Math.random() * 1000)}`; // Unique key
-                  if (!dotStateExecutionLogDictionary[stateKey]) { 
-                    dotStateExecutionLogDictionary[stateKey] = [];  // Init dictionary
-                  }
-                  //Stuff goes here
-                    if (prop.format === 'date' || prop.format === 'date-time') {
-                      let isValid = false;
-                      isTrackRuleExecution = true; // Should be tracked for diagnostic
-                      if (prop.format === 'date' && prop.example !== undefined) {
-                          const pattern = /^([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/; // Correct date pattern according to RFC3339
-                          isValid = prop.example && pattern.test(prop.example);
-                      } else if (prop.format === 'date-time' && prop.example !== undefined) {
-                          isValid = prop.example && isValidRFC3339DateTime(prop.example);
-                      }
-                      if (!isValid) {
-                          const isDate = prop.format === 'date'; // State one of the fields date / date-time
-                          dotStateExecutionLogDictionary[stateKey].push({
-                              schemaKey: key,
-                              propertyName: prop.name,
-                              propertyType: prop.type,
-                              propertyFormat: prop.format,
-                              propertyExample: prop.example,
-                              propertyValidFormatDate: isDate ? false : true,
-                              propertyValidFormatDateTime: !isDate ? false : true
-                          });
-                      }
-                    }
-                });
-              }
-              for (const stateKey in dotStateExecutionLogDictionary) {
-                const properties = dotStateExecutionLogDictionary[stateKey];
-                const hasInvalidFormat = properties.some(property => !(property.propertyValidFormatDate && property.propertyValidFormatDateTime));
-                if (hasInvalidFormat) {
-                    properties.forEach(property => {
-                        result.push({
-                            path: [...paths.path, property.schemaKey, "properties", property.propertyName,"example"],
-                            message: this.message,
-                            severity: this.severity
-                        });
-                    });
+export class Dot02 extends DotRuleBase {
+  static customProperties: CustomProperties = {
+    område: "Datum- och tidsformat",
+    id: "DOT.02",
+  };
+  description = "Ett giltigt exempel enligt DOT.02 behöver anges som ett exempel";
+  message = "Datum och tid SKALL anges enligt RFC 3339 som bygger på ISO-8601.";
+  then = {
+    function: (targetVal: any, _opts: string, paths) => {
+      const dotStateExecutionLogDictionary: DotStateExecutionLog = {};
+        const data = targetVal;
+        const isTrackRuleExecution = {value: false}; // Declate init state and set ByRef
+
+        for (const key in data) {
+            const properties: Property[] = parseProperties(key, data);
+            properties.forEach(prop => {
+                const stateKey = `${key}:${prop.name}:${Math.floor(Math.random() * 1000)}`;
+                if (!dotStateExecutionLogDictionary[stateKey]) {
+                    dotStateExecutionLogDictionary[stateKey] = [];
                 }
-              }
-              if (isTrackRuleExecution) {
-                this.trackRuleExecutionHandler(JSON.stringify(targetVal,null,2), _opts, paths,this.severity,
-                this.constructor.name, moduleName,Dot02.customProperties);
-              }    
-            return result;
-          }
+                this.executeRuleLogic(prop, stateKey, dotStateExecutionLogDictionary, isTrackRuleExecution,Dot02.customProperties);
+            });
         }
-      severity = DiagnosticSeverity.Error;
+        return this.handleResult(dotStateExecutionLogDictionary, paths, moduleName, isTrackRuleExecution,Dot02.customProperties);
+    }
+  };
+  generateDateTimePattern(): string {
+    return super.generateDateTimePattern();
+  }
+  generateDatePattern(): string {
+    return super.generateDatePattern();
+  }
+
+  isValidWithOffset(example: string): boolean {
+    return true; // Always valid offset in DOT.02
+  }
 }
-export default { Dot02 };
+
+export class Dot03 extends DotRuleBase {
+
+  static customProperties: CustomProperties = {
+    område: "Datum- och tidsformat",
+    id: "DOT.03",
+  };
+  description = "När man använder RFC 3339 format BÖR tidszonen anges.";
+  message = "När man använder RFC 3339 format BÖR tidszonen anges.";
+  severity: DiagnosticSeverity = DiagnosticSeverity.Warning;
+
+
+  then = {
+    function: (targetVal: any, _opts: string, paths) => {
+      const dotStateExecutionLogDictionary: DotStateExecutionLog = {};
+        const data = targetVal;
+        const isTrackRuleExecution = {value:false};
+        for (const key in data) {
+            const properties: Property[] = parseProperties(key, data);
+            properties.forEach(prop => {
+                const stateKey = `${key}:${prop.name}:${Math.floor(Math.random() * 1000)}`;
+                if (!dotStateExecutionLogDictionary[stateKey]) {
+                    dotStateExecutionLogDictionary[stateKey] = [];
+                }
+                this.executeRuleLogic(prop, stateKey, dotStateExecutionLogDictionary, isTrackRuleExecution,Dot03.customProperties);
+              });
+        }
+        return this.handleResult(dotStateExecutionLogDictionary, paths, moduleName, isTrackRuleExecution,Dot03.customProperties);
+    }
+
+  };
+  generateDateTimePattern(): string {
+    return super.generateDateTimePattern();
+  }
+  generateDatePattern(): string {
+    return super.generateDatePattern();
+  }
+  isValidWithOffset(example: string): boolean {
+    const offsetIndex = example.lastIndexOf('+') || example.lastIndexOf('-');
+    if (offsetIndex !== -1) {
+        const offsetPart = example.substring(offsetIndex);
+        return true; // Offset found
+    } else {
+        return false; // No offset found
+    }
+  }
+}
+
+export default { Dot02,Dot03};
