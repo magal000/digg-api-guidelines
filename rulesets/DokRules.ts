bb4d664d2e0de6894ed2e046a9e927cd9ad8b0b5
MySpace78 <ajmania786@gmail.com>
2024-07-01 17:59:23 +0200

    Dok03 rules fixed

--- rulesets/DokRules.ts
+++ rulesets/DokRules.ts
@@ -168,4 +168,109 @@ export class Dok01 extends BaseRuleset {
   severity = DiagnosticSeverity.Warning; 
 }
 
-export default { Dok23, Dok20, Dok19, Dok07 , Dok01};
+export class Dok03 extends BaseRuleset {
+  static customProperties: CustomProperties = {
+    område: "Dokumentation",
+    id: "DOK.03",
+  };
+  description = "Dokumentationen för ett API SKALL (DOK.03) innehålla följande";
+  message =  this.description + "nfo ,title, description ,contact ,email ,license, url: version";
+  given = "$";
+  then = [{
+  function:(targetVal, _opts, paths) => {
+        let info:any = [];
+        const url:string = 'url';
+        const email:string = 'email';
+        const name:string = 'name';
+  
+        if (targetVal.hasOwnProperty('info')) {   
+          this.trackRuleExecutionHandler(JSON.stringify(targetVal,null,2), _opts, paths,this.severity,
+          this.constructor.name, moduleName,Dok03.customProperties);
+                
+          info = targetVal['info']
+          if(info['version'] && info['title'] && info['description'] && info['contact'] && info['license']) {
+            
+            const license = info['license']
+            
+         if(license === null) {
+            return [{
+              path: ['info', 'license'],
+              message: this.message,
+              severity: this.severity
+            }]
+           } else {
+                  if(!license.hasOwnProperty('name') || !license.hasOwnProperty('url')){
+                  return [ {
+                    path: ['info' , 'license'],
+                    message: this.message,
+                    severity: this.severity
+                  }]              
+                
+              }
+              }
+        
+          const contact = info['contact']   
+          console.log("contact ==" , contact)
+          if(contact===null) {
+          return [{
+              path: ['info', 'contact' ],
+              message: this.message,
+              severity: this.severity
+            }]
+                     
+          } else {
+            const contactKeys = Object.keys(contact);
+            console.log("contactKeys object" ,contactKeys)
+            for(let i=0; i<contactKeys.length; i++){
+              console.log("i == " + i + " " + contact.hasOwnProperty(name));
+              if(!contact.hasOwnProperty(name)){
+                return [ {
+                  path: ['info', 'contact' ,i,  name],
+                  message: this.message,
+                  severity: this.severity
+                }] 
+              }
+              if(!contact.hasOwnProperty(email)){
+                return [ {
+                  path: ['info', 'contact' ,i,  email],
+                  message: this.message,
+                  severity: this.severity
+                }] 
+              }
+
+              if(!contact.hasOwnProperty(url)){
+                return [ {
+                  path: ['info', 'contact' ,i,  url],
+                  message: this.message,
+                  severity: this.severity
+                }] 
+              }
+
+            }
+         
+        }
+      
+  
+      } else{
+   
+        return [{
+          path: [targetVal ],
+          message: this.message,
+          severity: this.severity
+        }]
+      }     
+    } }
+  },
+      
+  
+  {
+    function: (targetVal: string, _opts: string, paths: string[]) => {
+      this.trackRuleExecutionHandler(JSON.stringify(targetVal, null, 2), _opts, paths,
+        this.severity, this.constructor.name, moduleName, Dok03.customProperties);
+    }
+  }
+  ];
+ severity = DiagnosticSeverity.Error;
+}
+
+export default { Dok23, Dok20, Dok19, Dok07 , Dok01, Dok03};
