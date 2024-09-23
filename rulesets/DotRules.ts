fa4804242881b1976aa218bc986bade74f854fe4
lofms <mats.lofstrand@arbetsformedlingen.se>
2024-09-23 09:12:11 +0200

    updated message on dot01

--- rulesets/DotRules.ts
+++ rulesets/DotRules.ts
@@ -12,19 +12,22 @@ export class Dot01 extends BaseRuleset {
     område: "Filtrering, paginering och sökparametrar",
     id: "DOT.01",
   };
-  description = "todo";
-  message = "todo";
-  given = "dasdsa";
+  description = "Datum och tid SKALL (DOT.01) hanteras enligt följande, använd alltid RFC 3339 för datum och tid, acceptera alla tidszoner i API:er returnera datum och tid i UTC och använd inte tidsdelen om du inte behöver den.";
+  message = "Datum och tid SKALL hanteras enligt följande, använd alltid RFC 3339 för datum och tid, acceptera alla tidszoner i API:er returnera datum och tid i UTC och använd inte tidsdelen om du inte behöver den.";
+  given = "$..responses..content.application/json.schema.properties";
   then = [{
       function:  (targetVal: any, _opts: string, paths) => {
+        
         const result:any = [];
         const regexp:RegExp = /^(\d{4})-(0[1-9]|1[0-2])-([0-2][0-9]|3[0-1])T([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])(\.\d{1,6})?(Z|([+-])([0-1][0-9]|2[0-3]):([0-5][0-9]))?Z$/
         for (const [key] of Object.entries(targetVal)){
-     
-          if(targetVal[key].hasOwnProperty("format")){
-            if (targetVal[key].format == "date-time" && !regexp.test(targetVal[key].example) ){
-              paths.path.push(key)
-              result.push(this)
+          console.log(targetVal[key])
+          if(targetVal[key].hasOwnProperty("format") && targetVal[key].format == "date-time"){
+            
+            if (!regexp.test(targetVal[key].example) ){
+              
+              paths.path.push(key);
+              result.push(this);
             }
 
           }
--- tests/dot.test.ts
+++ tests/dot.test.ts
@@ -119,7 +119,7 @@ testRule("Dot02", [
         },
       ],
     }]);
-    testRule("Dot03", [
+    testRule("Dot01", [
       {
         name: "giltigt testfall - date/datetime",
         document: {
@@ -141,32 +141,4 @@ testRule("Dot02", [
           },
         },
         errors: [],
-      },
-      {
-        name: "ogiltigt testfall - date/date-time",
-        document: {
-          openapi: "3.1.0",
-          info: { version: "1.0" },
-          components: {
-            schemas: {
-              DateRequest: {
-                type: "object",
-                properties: {
-                  "postedTime": {
-                      type: "string",
-                      format: "date-time",
-                      example: "2024-04-23T04:00:09.12" // No offset specified
-                    },
-                  },
-              },
-            },
-          },
-        },
-        errors: [
-          {
-            message: "Tidzonen BÖR (DOT.04) representeras med UTC formatet, där tid anges som offset från UTC (Coordinated Universal Time).",
-            path: ["components", "schemas", "DateRequest","properties","postedTime","example"],
-            severity: DiagnosticSeverity.Warning,
-          },
-        ],
-      }]);
+      },])
