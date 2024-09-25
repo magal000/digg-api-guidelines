45c746119560af0195549cf89add3b100902a0e8
Fredrik Nordlander <fredrik.nordlander@digg.se>
2024-09-25 13:28:53 +0200

    Tagit bort console.log från regelklassen

--- rulesets/DotRules.ts
+++ rulesets/DotRules.ts
@@ -16,7 +16,6 @@ export class Dot01 extends BaseRuleset {
   given = "$..responses..content.application/json.schema.properties";
   then = [{
       function:  (targetVal: any, _opts: string, paths) => {
-        console.log(targetVal)
         const result:any = [];
         const regexp:RegExp = /^(\d{4})-(0[1-9]|1[0-2])-([0-2][0-9]|3[0-1])T([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])(\.\d{1,6})?(Z|([+-])([0-1][0-9]|2[0-3]):([0-5][0-9]))?Z$/
         for (const [key] of Object.entries(targetVal)){
