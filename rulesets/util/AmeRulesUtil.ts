2a60a526ae69199e90fef1f3f8008a324b6188f8
Magnus Almin <magnus.almin@noreply.com>
2025-05-25 14:26:57 +0200

    [Cherry-pick from fork] #321: Remove rest of probably unnecessary stuff.

--- rulesets/util/AmeRulesUtil.ts
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2025 diggsweden/rest-api-profil-lint-processor
-//
-// SPDX-License-Identifier: EUPL-1.2
-
-export function isValidApplicationJson(property: string): boolean {
-  return !!property.match(/application\/json(?:$|;[\s]*charset=.*)/);
-}
