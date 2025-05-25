085cbd5ffa081fc4b40675faf7d0427d1a0d8b3c
Magnus Almin <magnus.almin@noreply.se>
2025-05-25 10:59:49 +0200

    [Cherry-pick from fork] #321: Started deleting superflous stuff. I suspect the whole node.js application is unnecessary and this could simply be a spectral ruleset file. Keep rules and test cases for now since they will have to be moved to spectral.yaml file

--- ruleinterface/CustomProperties.ts
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2025 diggsweden/rest-api-profil-lint-processor
-//
-// SPDX-License-Identifier: EUPL-1.2
-
-// Define a type for CustomProperties
-export interface CustomProperties {
-  område: string;
-  id: string;
-}
